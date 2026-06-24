7d7b5e0bf0a324da47444256523bd593cfb1c8f3
arq7arq <amirrezaghasemi899@gmail.com>
2026-06-24 07:33:09 +0330

    LoginForm Design

---
+++ Tahlilgaran/Forms/LoginForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Tahlilgaran.Forms
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbUsername = new System.Windows.Forms.TextBox();
+            this.txbPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txbUsername
+            // 
+            this.txbUsername.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txbUsername.Location = new System.Drawing.Point(101, 290);
+            this.txbUsername.Name = "txbUsername";
+            this.txbUsername.PlaceholderText = "نام کاربری";
+            this.txbUsername.Size = new System.Drawing.Size(282, 51);
+            this.txbUsername.TabIndex = 0;
+            this.txbUsername.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txbPassword
+            // 
+            this.txbPassword.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txbPassword.Location = new System.Drawing.Point(101, 370);
+            this.txbPassword.Name = "txbPassword";
+            this.txbPassword.PlaceholderText = "رمزعبور";
+            this.txbPassword.Size = new System.Drawing.Size(282, 51);
+            this.txbPassword.TabIndex = 1;
+            this.txbPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnLogin.Location = new System.Drawing.Point(101, 450);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(282, 64);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "ورود";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Tahlilgaran.Properties.Resources.tahlilgaran1;
+            this.pictureBox1.Location = new System.Drawing.Point(47, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(397, 157);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(497, 535);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txbPassword);
+            this.Controls.Add(this.txbUsername);
+            this.Name = "LoginForm";
+            this.Text = "صفحه ورود";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txbUsername;
+        private TextBox txbPassword;
+        private Button btnLogin;
+        private PictureBox pictureBox1;
+    }
+}
