dffd36fc03b96d0ddf2344b420a8db0236065355
arq7arq <amirrezaghasemi899@gmail.com>
2026-06-25 17:35:05 +0330

    bug fix

--- Tahlilgaran/Properties/Resources.Designer.cs
+++ Tahlilgaran/Properties/Resources.Designer.cs
@@ -83,16 +83,6 @@ namespace Tahlilgaran.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap logo3 {
-            get {
-                object obj = ResourceManager.GetObject("logo3", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap project_management {
             get {
                 object obj = ResourceManager.GetObject("project-management", resourceCulture);
